eaf1216724f97506b10e2b0844c1d970fe5b3af4
Owen <grahamo@wncc.edu>
2026-01-22 10:42:34 -0700

    Changed approach to making window into oop

--- GameFrame.java
+++ GameFrame.java
@@ -5,30 +5,29 @@ import javax.swing.*;
 
 import static java.lang.System.*;
 
-public class GameFrame {
-    public static void main(String[] args) {
-        // Create a JFrame named gameFrame for all of the game components
-        JFrame gameFrame = new JFrame("Snake");
-        // Set size and properties.
-        gameFrame.setSize(600, 600);
-        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gameFrame.setResizable(false);
-        gameFrame.setVisible(true);
-        gameFrame.getContentPane().setBackground(Color.LIGHT_GRAY);
-        // create a panel to hold button1
+public class GameFrame extends JFrame {
+    private void init() {
+        this.setSize(300, 300);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setVisible(true);
+        this.getContentPane().setBackground(Color.LIGHT_GRAY);
         JPanel panel = new JPanel();
-        // create button1
-        JButton button1 = new JButton();
-        // add panel and button to the main frame
-        gameFrame.add(panel);
+        JButton button1 = new JButton("START");
+        this.add(panel);
         panel.add(button1);
-        // make sure gameFrame is visible
-        gameFrame.setVisible(true);
-        // Set an action listener so the button shows a message when pressed
+        this.setLayout(new FlowLayout());
+        this.setMinimumSize(new Dimension(300, 300));
+        this.pack();
+        this.setVisible(true);
         button1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
-                JOptionPane.showMessageDialog(gameFrame.getComponent(0), "START");
+                JOptionPane.showMessageDialog(getComponent(0), "START");
             }
         });
+    }
+    public GameFrame() {
+        super("Snake");
+        init();
     }
 }
